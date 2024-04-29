0c062f06460a02fc081998b6bad9bcfa76a3a151
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2024-04-29 08:43:11 -0600

    404 tells you where you are

--- errors/404.php
+++ errors/404.php
@@ -1,5 +1,10 @@
 <?php
 header("Content-type: text/plain");
-echo "404 Not Found";
+
+$url =  $_SERVER['REQUEST_URI']; 
+$url = str_replace('%20', ' ', $url);
+
+echo "404 Not Found\n$url";
+
 die();
 ?>
