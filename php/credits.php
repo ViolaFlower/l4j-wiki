47a5ec4d1f5e6a2c7c00b58aada30f9268774fbe
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2024-06-03 18:38:16 -0600

    Update credits.php

--- php/credits.php
+++ php/credits.php
@@ -7,6 +7,7 @@
 Legacy 4J Developer <br><b><a href="https://modrinth.com/user/Wilyicaro">Wilyicaro</a> <img class="playerhead" src="/assets/playerheads/Wilyicaro.png"></b><br>
 Modpack Developer <br><b><a href="https://modrinth.com/user/omoso">omoso</a> <img class="playerhead" src="/assets/playerheads/omoso_.png"></b><br>
 Wiki Developer <br><b><a href="https://modrinth.com/user/bathtubfulloftoast">bathtubfulloftoast</a> <img class="playerhead" src="/assets/playerheads/bathtuboftoast.png"></b><br>
+Maintainer <br><b><a href="https://modrinth.com/user/devin">devin</a> <img class="playerhead" src="/assets/playerheads/devinreal.png"></b><br>
 Playtester <br><b><a href="https://modrinth.com/user/BuddarScotchy">BuddarScotchy</a> <img class="playerhead" src="/assets/playerheads/BuddarScotchy.png"></b><br>
 Playtester<br><b><a href="https://modrinth.com/user/theminecraftarchitect">The Minecraft Architect</a><img class="playerhead" src="/assets/playerheads/TheMCArchitect1.png"></b><br>
 </h3>
